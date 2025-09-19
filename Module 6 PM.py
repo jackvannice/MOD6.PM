6a46148226e65237f4996957a8097d94c0ac0525
jackvannice <jackvannice7@gmail.com>
2025-09-19 16:52:12 -0600

    Step 4 with all methods working and outputs as desired for assignment

--- Module 6 PM.py
+++ Module 6 PM.py
@@ -5,10 +5,11 @@
 class ItemToPurchase:
 
     #default constructor
-    def __init__(self, item_name = "none", item_price = 0.0, item_quantity = 0):
+    def __init__(self, item_name = "none", item_price = 0.0, item_quantity = 0, item_description = ""):
         self.item_name = item_name
         self.item_price = item_price
         self.item_quantity = item_quantity
+        self.item_description = item_description
 
     #method
     def print_item_cost(self):
@@ -21,11 +22,13 @@ print("Please enter item information for two items below ")
 name_input1 = input("Please enter item 1 name: ")
 price_input1 = float(input("Please enter item 1 price: "))
 quantity_input1 = int(input("Please enter item 1 quantity: "))
+desc_input1 = str(input("Please enter item 1 description: "))
 name_input2 = input("Please enter item 2 name: ")
 price_input2 = float(input("Please enter item 2 price: "))
 quantity_input2 = int(input("Please enter item 2 quantity: "))
-item1 = ItemToPurchase(name_input1, price_input1, quantity_input1)
-item2 = ItemToPurchase(name_input2, price_input2, quantity_input2)
+desc_input2 = str(input("Please enter item 2 description: "))
+item1 = ItemToPurchase(name_input1, price_input1, quantity_input1, desc_input1)
+item2 = ItemToPurchase(name_input2, price_input2, quantity_input2, desc_input2)
 
 #Step 3: Add costs and output total cost
 #print("TOTAL COST")
@@ -73,18 +76,26 @@ class ShoppingCart:
             return
         print(f"{self.customer_name}'s shopping cart - {self.current_date}")
         print(f"Number of items: {self.get_num_items_in_cart()}")
-        print()
 
         for item in self.cart_items:
             item.print_item_cost()
-
-        print()
         print(f"total: ${self.get_cost_of_cart()}")
+
+    def print_descriptions(self):
+        if len(self.cart_items) == 0:
+            print("Shopping cart is empty.")
+            return
+        print(f"{self.customer_name}'s shopping cart - {self.current_date}")
+        print('Item descriptions: ')
+        for item in self.cart_items:
+            print(f"{item.item_name}: {item.item_description}")
+
 my_shopping_cart = ShoppingCart("John" , "February 1, 2020")
 
 my_shopping_cart.add_item(item1)
 my_shopping_cart.add_item(item2)
 
 my_shopping_cart.print_total()
+my_shopping_cart.print_descriptions()
 
 
